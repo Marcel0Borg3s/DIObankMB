7a4763d494ef664cfb00290315c331e48a47323a
marcelo_borges <marcelosbgs@gmail.com>
2025-03-27 21:02:38 -0300

    inclusão de todas funçções de deposósito, saque e extrato

--- main.py
+++ main.py
@@ -1,29 +1,67 @@
 
-
 saldo = 0
-saque = 0
+saque_limite = 0
 extrato = []
 
-print("Bem vindo ao Banco MB")
+print("\nBem vindo ao MBank\n")
 
 while True:
-    print("## Menu Principal ##")
+    print("## Menu Principal ##\n")
     print("Escolha uma opção: ")
     print("S - Saque")
     print("D - Depósito")
     print("E - Extrato")
-    print("S - Saldo")
-    print("X - Sair")
+    print("X - Sair\n")
 
     opcao = input("Digite a opção desejada: ").upper()
 
+    # SAQUE
     if opcao == "S":
-        float(input("Opção escolhida = SAQUE\nLimite de R$500,00 por saque\nDigite o valor desejado: R$ "))
+        saque = float(input("\nOpção escolhida = SAQUE\nLimite de R$500,00 por saque\nDigite o valor desejado: R$"))
         if saldo < saque:
-            print("Saldo insuficiente")
-            
+            print(">>> Saldo insuficiente <<<")
+        elif saque > 500:
+            print("\nValor do saque excede o limite de R$500,00")
+        elif saque_limite >= 3:
+            print("\n>>> Limite de saque atingido <<<")
+            break
         else:
-            print("Saque realizado com sucesso")
+            saldo -= saque
+            saque_limite += 1
+            extrato.append(f"Saque de R$ {saque:.2f}")
+            print("\nSaque realizado com sucesso")
+        print(f">>> \nSaldo atualizado: R$ {saldo:.2f}")
+    
+    # DEPÓSITO
+    elif opcao == "D":
+        deposito = float(input("\nOpção escolhida = DEPÓSITO\nDigite o valor a depositar: R$"))
+        saldo += deposito
+        extrato.append(f"\nDepósito de R$ {deposito:.2f}")
+        print(f"\n>>> Saldo atualizado: R$ {saldo:.2f}")
+
+    # EXTRATO
+    elif opcao == "E":
+        if not extrato:
+            print("\n>>> Nenhuma transação realizada <<<")
+            print(f">>> Saldo atualizado: R$ {saldo:.2f}")
+
+        else:
+            print("\nExtrato:")
+            for item in extrato:
+                print(item)
+            print(f"\n>>> Saldo atualizado: R$ {saldo:.2f}")
+
+    elif opcao == "X":
+        print("\n>>> Sessão encerrada <<<")
+        break
+
+    else:
+        print("\n>>> Opção inválida <<<")   
+    
+
+    
+    
+    
 
 
 
